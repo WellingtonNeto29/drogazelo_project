50eb9d9aa1f112697fedc2f8d711737573bca4d3
Wellington Neto <wellingtonmoreiraneto@hotmail.com>
2025-12-14 04:07:29 -0300

    adiciona migrações para o modelo de Pedido e ajusta a configuração de arquivos estáticos; inclui novo estilo CSS e logo

--- drogazelo/drogazelo/settings.py
+++ drogazelo/drogazelo/settings.py
@@ -111,6 +111,9 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+# Para ambientes de produção, adicione:
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
