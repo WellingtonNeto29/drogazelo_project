5e6882752c2ab16d00de999c53d10c42741cb31b
Wellington Neto <wellingtonmoreiraneto@hotmail.com>
2025-12-13 23:25:47 -0300

    user registration and login functionalities, template updates, and styling.

--- drogazelo/produtos/views.py
+++ drogazelo/produtos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Produto
+from django.shortcuts import render, redirect
+
 
 def lista_produtos(request):
     produtos = Produto.objects.all()
--- drogazelo/usuarios/views.py
+++ drogazelo/usuarios/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from .forms import CadastroForm
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 
 def cadastro(request):
     if request.method == 'POST':
-        form = CadastroForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = User.objects.create_user(
-                username=form.cleaned_data['username'],
-                email=form.cleaned_data['email'],
-                password=form.cleaned_data['senha']
-            )
-            login(request, user)
-            return redirect('lista_produtos')
+            form.save()
+            return redirect('/login/')
     else:
-        form = CadastroForm()
+        form = UserCreationForm()
+
     return render(request, 'usuarios/cadastro.html', {'form': form})
+
+def login_view(request):
+    erro = False
+
+    if request.method == 'POST':
+        usuario = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(request, username=usuario, password=senha)
+
+        if user:
+            login(request,)
