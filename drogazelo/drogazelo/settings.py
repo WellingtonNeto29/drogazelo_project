c54ab1cc4429464c29614d05ec964c8bcd10ecba
Wellington Neto <wellingtonmoreiraneto@hotmail.com>
2025-12-13 21:53:10 -0300

    adding initial migrations for the product and profile models, and creating a product list template

--- drogazelo/drogazelo/settings.py
+++ drogazelo/drogazelo/settings.py
@@ -55,12 +55,13 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'drogazelo.urls'
 
 TEMPLATES = [
-    {
+   {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],  # <- TEM que ter isso
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
